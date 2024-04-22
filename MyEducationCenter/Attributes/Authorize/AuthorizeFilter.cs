a2ee7b8e1b5b8732a41fb0da69703e946e4230e8
Jamol Meyliyev <113878950+JamolMeyliyev@users.noreply.github.com>
2024-04-22 17:32:39 +0500

    add

---
+++ MyEducationCenter/Attributes/Authorize/AuthorizeFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using MyEducationCenter.Core;
+
+namespace MyEducationCenter.Attributes;
+public class AuthorizeFilter : Attribute, IAuthorizationFilter, IFilterMetadata
+{
+    protected string[] ModuleCodes { get; }
+
+    public AuthorizeFilter(string[] moduleCodes)
+    {
+        ModuleCodes = moduleCodes;
+    }
+
+    public virtual void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!context.Filters.Any((IFilterMetadata a) => a is AllowAnonymousAttribute))
+        {
+            IAuthService authService = (IAuthService)context.HttpContext.RequestServices.GetService(typeof(IAuthService));
+            if (!authService.IsAuthenticated)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+            else if ((ModuleCodes?.Any()).GetValueOrDefault() && !ModuleCodes.Any((string moduleCode) => authService.HasPermission(moduleCode)))
+            {
+                context.Result = new StatusCodeResult(403);
+            }
+        }
+    }
+}
